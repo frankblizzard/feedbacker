7b0f3a762fba28c56bf6bfed5084bbddd2f98b9e
Thomas <thomasmaximini@gmx.de>
2011-01-27 17:14:34 +0100

    rails install, gem setup, basic layout & authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedbacker::Application.config.secret_token = '32e532533370fc71788cf9080732887a4bd3fcc8a81461e59d1a1c465a0e7b916136b18209419bb06fa86f7bedb29bd28728752c9c15d5ff74c49de537f99703'
---
+++ public/javascripts/application.js
@@ -0,0 +1,14 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function () {
+	 calculateSizes();
+	 $(window).resize(function () {
+		 calculateSizes();
+	 });
+});
+
+calculateSizes = function () {
+	 $(".main").height($(window).height()-$('.header').height());
+	 $(".right_side").width($(window).width()-$('.left_side').width());
+}
