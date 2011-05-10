881faa129e6d3bfe0f8c153eee3c9286fe416da2
Thomas <thomasmaximini@gmx.de>
2011-05-10 09:55:45 +0200

    test cron job

---
+++ config/schedule.rb
@@ -0,0 +1,25 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :output, "/log/cron_log.log"
+
+every 1.day, :at => "10am" do
+  dump_path = "/User/eveadmin/rails/db_backups/dump#{Date.today.to_s}"
+  command "sudo mysqldump -uroot -prootz feedbacker_development > #{dump_path}.sql"
+  command "tar -zcvf #{dump_path}.tar.gz #{dump_path}.sql"
+  command "rm #{dump_path}.sql"
+
+  runner "Storage.store_dump '#{dump_path}.tar.gz'"
+end
+
+
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
