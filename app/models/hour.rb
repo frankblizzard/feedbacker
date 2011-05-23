4b0d831736e1f6e9b244e9ebb6d09460b3c06561
Thomas <thomasmaximini@gmx.de>
2011-05-23 11:56:10 +0200

    home bug fixed: category_id now obligatory for new hours

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -6,6 +6,7 @@ class Hour < ActiveRecord::Base
     validates_presence_of :task
     validates_presence_of :project_id
     validates_presence_of :user_id
+    validates_presence_of :work_category_id
     
     belongs_to :user
     belongs_to :project
