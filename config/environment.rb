6ec97e7e36d74a7b36dcc69fca1f7bb08f8e99d9
Thomas <thomasmaximini@gmx.de>
2011-05-23 12:12:14 +0200

    paginate and order projects

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActiveRecord::Base.instance_eval do
+  def 
+    per_page; 15; 
+  end
+end
+
 # Initialize the rails application
 Feedbacker::Application.initialize!
