caef40e4b843a737d4ea317c3e392b46c6fea4d4
Thomas <thomasmaximini@gmx.de>
2011-05-25 17:13:29 +0200

    bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :login_required, :except => [:new, :create]
+  before_filter :login_required
   
   def index
     @users = User.where("name like ?", "%#{params[:q]}%")
